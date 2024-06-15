66433af54a99edcf1af559d047427f1f6ac7d496
Yash Patel <yash1451999@gmail.com>
2024-06-15 19:37:49 -0400

    logo Updated

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,23 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const Logo = () => {
   return (
     <div className="relative z-20 flex items-center text-lg font-medium  ">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z"
-        />
-      </svg>
-      Project Manager
+      <Image src="/logo.png" height={50} width={50} alt="logo"></Image>
+      <h1 className="bg-gradient-to-r from-yellow-600 via-yellow-500 to-yellow-400 inline-block text-transparent bg-clip-text">
+        Brighter Bee
+      </h1>
     </div>
   );
 };
